19abd338cb4b07b977dba91992aee79c46137fef
Milan Oberkirch <milan@oberkirch.org>
2013-09-01 17:46:18 +0200

    Made setup_user_configs.bash more careful.

--- setup_user_configs.bash
+++ setup_user_configs.bash
@@ -1,3 +1,5 @@
+__NAME__="setup_user_configs"
+
 function setup_user_configs() {
   declare -A local self=(
     [name]="$__NAME__"
@@ -16,13 +18,18 @@ function setup_user_configs() {
     [xinitrc]="$HOME/.xinitrc"
     [xprofile]="$HOME/.xprofile"
     [.bashrc]="$HOME/.bashrc"
+    [vim/vimrc.local]="$HOME/.vimrc.local"
+    [vim/vimrc.bundles.local]="$HOME/.vimrc.bundles.local"
     )
 
   for target in ${!locations[@]}; do
-    ln -s ${self[location]}/$target ${locations[$target]}
+    ln --symbolic --interactive --no-target-directory\
+      ${self[location]}/$target ${locations[$target]}
   done
 
   cat ${self[location]}/ssh/* >> $HOME/.ssh/authorized_keys
 }
 
-setup_user_configs $0
+if [[ $0 == *setup_user_configs.bash ]]; then
+  setup_user_configs $0 $*
+fi
