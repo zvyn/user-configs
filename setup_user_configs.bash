bba9a279d2754fbc58589bbc697c96c13376cc0f
Milan Oberkirch <milan@oberkirch.org>
2013-11-07 13:31:02 +0100

    minor changes

--- setup_user_configs.bash
+++ setup_user_configs.bash
@@ -75,7 +75,7 @@ function setup_user_configs() {
 
 ## Run only if called directly. Makes sourcing of this file safe.
 if [ ! $__NAME__ ]; then
-  __NAME__=$(basename --suffix=.bash $0)
+  __NAME__=$(basename -s .bash $0)
   if [[ $(type -t ${__NAME__}) == function ]]; then
     ${__NAME__} $0 $*
   fi
