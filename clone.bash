3e77a1bddbe51d509090d2e92ec2b0c03a6e154d
Milan Oberkirch <milan@oberkirch.org>
2014-04-08 12:37:57 +0200

    Update text.

--- clone.bash
+++ clone.bash
@@ -2,20 +2,7 @@
 # Copyright: Milan Oberkirch, 2013 (CC BY-SA 3.0).
 
 function clone_setup() {
-## Clone user-configs from github and run setup.
-  echo Clone user-configs to $HOME/config and run setup-script? [y/N]\ 
-  local desition=''
-  read desition
-  case $desition in
-    y|yes|Y|Yes)
-      set -e
-      git clone https://github.com/zvyn/user-configs.git $HOME/config
-      ($HOME/config/setup_user_configs.bash)
-      ;;
-    *)
-      return 1
-      ;;
-  esac
-#/>
+    git clone https://github.com/zvyn/user-configs.git $HOME/config
+    echo "Run $HOME/config/setup_user_configs.bash to install the new configuration."
 }
 clone_setup
