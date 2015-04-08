abd9181bd2dc0bb5086e0a0d978a7037b9e86624
Milan Oberkirch <zvyn@oberkirch.org>
2015-04-08 13:18:22 +1200

    source /usr/share/doc/pkgfile/command-not-found.bash

--- shell/config.bash
+++ shell/config.bash
@@ -90,6 +90,8 @@ complete -f -o default -X '!*.tex' tex latex pdflatex
 
 PROMPT_COMMAND=prompt_command
 
+source /usr/share/doc/pkgfile/command-not-found.bash
+
 ((_HOME_CONFIG_BASH_PROFILE_+=1))
 export _HOME_CONFIG_BASH_PROFILE_
 
