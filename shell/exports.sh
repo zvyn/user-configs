6dff8cc5f525f44c8c46dfed0a978a8efc4c1e7d
zvyn <@zvyn.eu>
2021-10-16 20:00:08 +0200

    Save aliases and exports separately for fish

---
+++ shell/exports.sh
@@ -0,0 +1,21 @@
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_TIME="de_DE.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_MONETARY="de_DE.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export MM_CHARSET="de_DE.UTF-8"
+
+export HISTIGNORE="&:ls:[bf]g:exit: *:reboot:poweroff"
+export HISTFILE="$HOME/.histfile"
+export HISTSIZE=5000
+export SAVEHIST=5000
+export EDITOR="nvim"
+export GOPATH=$HOME/go
+export PATH="/usr/lib/colorgcc/bin:$PATH:/home/milan/Programme/bin:/home/milan/.gem/ruby/2.0.0/bin:$GOPATH/bin"
+export LS_COLORS='di=01;36:*.zip=33:*.tar=33:*.tar.gz=33:*.tar.bz2=33:*.jpg=35:*.JPG=35:*.jpeg=35:*.JPEG=35:*.PNG=35:*.png=35:*.rar=33:*.ico=35:*.gif=35:*.svg=35:*.xcf=35:*.cpp=31:*.h=31:*.rkt=31'
+export QEMU_AUDIO_DRV=pa
+export CDPATH=~/Code
+export PIP_REQUIRE_VIRTUALENV=1
